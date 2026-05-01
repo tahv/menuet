3f703dea805b0db196d85c6205b74de50511377b
Thibaud Gambier <account+gitlab@tgambier.fr>
2026-05-01 21:52:02 +0200

    Format demo_maya.py

--- docs/assets/demo_maya.py
+++ docs/assets/demo_maya.py
@@ -2,9 +2,5 @@ from menuet.builders.maya import MayaMenuBuilder
 from menuet.demo import demo_model
 
 model = demo_model()
-builder = MayaMenuBuilder(
-    model,
-    root_menu="Demo",
-    parent="MayaWindow",
-)
+builder = MayaMenuBuilder(model, root_menu="Demo", parent="MayaWindow")
 builder.build()
