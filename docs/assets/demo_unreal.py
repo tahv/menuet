204d86409f02ce1f9075147279447c08f3424b0d
Thibaud Gambier <account+gitlab@tgambier.fr>
2026-07-17 22:27:54 +0200

    refactor!: remove helper function `model_reference_to_string_command` (tahv/menuet!9)

--- docs/assets/demo_unreal.py
+++ docs/assets/demo_unreal.py
@@ -1,14 +1,36 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import unreal
 
-from menuet.builders.unreal import UnrealMenuBuilder, model_reference_to_string_command
+from menuet.builders.unreal import UnrealMenuBuilder
 from menuet.demo import demo_model
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+
+def to_string_command_factory(model: str) -> Callable[[str], str]:
+    """Generate `to_string_command` argument from a `Model` reference."""
+
+    def inner(action: str) -> str:
+        return f"""\
+        from importlib.metadata import EntryPoint
+        model_loader = EntryPoint(name="", group="", value='{model}').load()
+        model = model_loader()
+        model.get_action('{action}').cb()
+        """
+
+    return inner
+
 
 tool_menus = unreal.ToolMenus.get()
 model = demo_model()
 builder = UnrealMenuBuilder(
     model,
     root_name="Demo",
-    to_string_command=model_reference_to_string_command("menuet.demo:demo_model"),
+    to_string_command=to_string_command_factory("menuet.demo:demo_model"),
     parent=tool_menus.find_menu(unreal.Name("LevelEditor.MainMenu")),
 )
 builder.build()
